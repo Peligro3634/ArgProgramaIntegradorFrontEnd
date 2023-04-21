49325b8752e83158d3e60b0c22add4e62b1ab0e7
Peligro Dev <111589151+Peligro3634@users.noreply.github.com>
2023-04-21 03:11:48 -0300

    avanzando

--- IntegradorAndresCeballosFront/src/app/app.module.ts
+++ IntegradorAndresCeballosFront/src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LogoAPComponent } from './header/logo-ap/logo-ap.component';
+import { SocialComponent } from './header/social/social.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
