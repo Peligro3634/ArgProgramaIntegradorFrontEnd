52c70195062251262502862c696a09507a5893c2
Peligro Dev <111589151+Peligro3634@users.noreply.github.com>
2023-04-23 18:20:59 -0300

    login ok

--- IntegradorAndresCeballosFront/src/app/header/logo-ap/logo-ap.component.ts
+++ IntegradorAndresCeballosFront/src/app/header/logo-ap/logo-ap.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo-ap',
   templateUrl: './logo-ap.component.html',
   styleUrls: ['./logo-ap.component.css']
 })
-export class LogoAPComponent {
+export class LogoAPComponent implements OnInit{
+
+  constructor(private router:Router){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+login(){
+  this.router.navigate(['/login'])
+}
 
 }
